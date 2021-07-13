e71f5f812b6cca032eb1eab6490f8df621cc3844
IT21111488 <87365163+IT21111488@users.noreply.github.com>
2021-07-13 09:55:44 +0000

    completed

--- Tute01.c
+++ Tute01.c
@@ -6,6 +6,21 @@
 
 int main() {
   
+  //variable delcaration
+  int n1 , n2;
+  float sum = 0, average = 0;
+
+  //aksing the user to enter marks
+  printf("Enter marks of 2 subjects : ");
+  scanf("%d%d",&n1, &n2);
+
+  //calculate sum and average
+  sum = n1 + n2;
+  average = sum / 2;
+
+  //printing the average
+  printf("Average : %.1f",average);
+  
   return 0;
 }
 
